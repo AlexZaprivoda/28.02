35bd767a7fc2fa8aa13863cdd6bd433e43767c78
AlexZaprivoda <zaprivoda.alexander@gmail.com>
2019-03-01 12:31:07 +0200

    3

--- index.js
+++ index.js
@@ -41,13 +41,11 @@
 
 // https://images.aif.ru/007/410/7099f160be2419d6128e3a57e62ec326.jpg
 
-
 // d1.addEventListener("click", onClick, true);
 // d2.addEventListener("click", onClick);
 // d3.addEventListener("click", onClick);
 // d4.addEventListener("click", onClick);
 // b1.addEventListener("click", onClick);
-
 
 // function onClick(e) {
 // 	console.log(this);
@@ -58,18 +56,323 @@
 // }
 const ROOT = document.querySelector("#container");
 
+let step = 0;
 
+ROOT.onclick = function(e) {
+  let x = document.createElement("p");
+  let o = document.createElement("p");
 
-ROOT.onclick = function (e) {
-	let x = document.createElement('p');
-	let o = document.createElement('p');
+  x.classList.add("x");
+  o.classList.add("o");
 
-	x.classList.add("x");
-	o.classList.add("o");
+  if (e.target.childNodes.length == 0 && e.target.tagName !== "P") {
+    if (step % 2 == 0) {
+      e.target.appendChild(o);
+      e.target.setAttribute("data-flag", "o");
+    } else {
+      e.target.appendChild(x);
+      e.target.setAttribute("data-flag", "x");
+    }
+    if (step == 5) {
+      //   setTimeout(() => {
+      alert("Anyone will can`t win");
+      location.reload();
 
-	if (e.target.childNodes.length == 0 && e.target.tagName !=="P"){
-		e.target.appendChild(o);
-	}
+      //   }, 10);
+    }
+    step++;
+    console.log(step);
+  }
+};
+
+function _win() {
+  //------------------ 1,2,3 O ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[3].getAttribute("data-flag") &&
+      ROOT.childNodes[3].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[5].getAttribute("data-flag") &&
+      ROOT.childNodes[5].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='2']").style.background = "green";
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 1,2,3 X ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[3].getAttribute("data-flag") &&
+      ROOT.childNodes[3].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[5].getAttribute("data-flag") &&
+      ROOT.childNodes[5].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='2']").style.background = "green";
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 4,5,6 O ------------------//
+  if (
+    ROOT.childNodes[7].getAttribute("data-flag") &&
+    ROOT.childNodes[7].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[11].getAttribute("data-flag") &&
+      ROOT.childNodes[11].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='4']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='6']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 4,5,6 X ------------------//
+  if (
+    ROOT.childNodes[7].getAttribute("data-flag") &&
+    ROOT.childNodes[7].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[11].getAttribute("data-flag") &&
+      ROOT.childNodes[11].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='4']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='6']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 7,8,9 O ------------------//
+  if (
+    ROOT.childNodes[13].getAttribute("data-flag") &&
+    ROOT.childNodes[13].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[15].getAttribute("data-flag") &&
+      ROOT.childNodes[15].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    ROOT.querySelector("[data-num='8']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 7,8,9 X ------------------//
+  if (
+    ROOT.childNodes[13].getAttribute("data-flag") &&
+    ROOT.childNodes[13].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[15].getAttribute("data-flag") &&
+      ROOT.childNodes[15].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    ROOT.querySelector("[data-num='8']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 1,4,7 O ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[7].getAttribute("data-flag") &&
+      ROOT.childNodes[7].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[13].getAttribute("data-flag") &&
+      ROOT.childNodes[13].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='4']").style.background = "green";
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 1,4,7 X ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[7].getAttribute("data-flag") &&
+      ROOT.childNodes[7].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[13].getAttribute("data-flag") &&
+      ROOT.childNodes[13].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='4']").style.background = "green";
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 2,5,8 O ------------------//
+  if (
+    ROOT.childNodes[3].getAttribute("data-flag") &&
+    ROOT.childNodes[3].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[15].getAttribute("data-flag") &&
+      ROOT.childNodes[15].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='2']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='8']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 2,5,8 X ------------------//
+  if (
+    ROOT.childNodes[3].getAttribute("data-flag") &&
+    ROOT.childNodes[3].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[15].getAttribute("data-flag") &&
+      ROOT.childNodes[15].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='2']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='8']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 3,6,9 O ------------------//
+  if (
+    ROOT.childNodes[5].getAttribute("data-flag") &&
+    ROOT.childNodes[5].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[11].getAttribute("data-flag") &&
+      ROOT.childNodes[11].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    ROOT.querySelector("[data-num='6']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 3,6,9 X ------------------//
+  if (
+    ROOT.childNodes[5].getAttribute("data-flag") &&
+    ROOT.childNodes[5].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[11].getAttribute("data-flag") &&
+      ROOT.childNodes[11].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    ROOT.querySelector("[data-num='6']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 1,5,9 O ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 1,5,9 X ------------------//
+  if (
+    ROOT.childNodes[1].getAttribute("data-flag") &&
+    ROOT.childNodes[1].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[17].getAttribute("data-flag") &&
+      ROOT.childNodes[17].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='1']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='9']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 3,5,7 O ------------------//
+  if (
+    ROOT.childNodes[5].getAttribute("data-flag") &&
+    ROOT.childNodes[5].getAttribute("data-flag") == "o" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "o") &&
+    (ROOT.childNodes[13].getAttribute("data-flag") &&
+      ROOT.childNodes[13].getAttribute("data-flag") == "o")
+  ) {
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN O!!!");
+      location.reload();
+    }, 50);
+  }
+
+  //------------------ 3,5,7 X ------------------//
+  if (
+    ROOT.childNodes[5].getAttribute("data-flag") &&
+    ROOT.childNodes[5].getAttribute("data-flag") == "x" &&
+    (ROOT.childNodes[9].getAttribute("data-flag") &&
+      ROOT.childNodes[9].getAttribute("data-flag") == "x") &&
+    (ROOT.childNodes[13].getAttribute("data-flag") &&
+      ROOT.childNodes[13].getAttribute("data-flag") == "x")
+  ) {
+    ROOT.querySelector("[data-num='3']").style.background = "green";
+    ROOT.querySelector("[data-num='5']").style.background = "green";
+    ROOT.querySelector("[data-num='7']").style.background = "green";
+    setTimeout(() => {
+      alert("WIN X!!!");
+      location.reload();
+    }, 50);
+  }
 }
 
-
+addEventListener("click", _win);
